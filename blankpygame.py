8d0fcada5c7545d97c9724d601de492f76a898a1
xzlxiao <xzlxiao@outlook.com>
2016-10-14 15:47:32 +0800

    blank

--- blankpygame.py
+++ blankpygame.py
@@ -2,11 +2,11 @@
 import pygame, sys
 from pygame.locals import *
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.init()	#pygame初始化
+DISPLAYSURF = pygame.display.set_mode((400, 300))	#返回窗口对象
 pygame.display.set_caption('Hello World!')
 while True: # main game loop 
-	for event in pygame.event.get():
+	for event in pygame.event.get():	#返回pygame.event.Event对象列表
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
